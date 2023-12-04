e70ed6acef7643975f6d1f01eff8dec4ca596bfa
urquico <kurtjacob.urquico@gmail.com>
2023-12-05 01:57:35 +0800

    [Feature][Kurt] - Added prettier and eslints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { Button, Text } from "@mantine/core";
 
 function Home() {
   return (
-    <div className="flex flex-col m-16 p-16">
+    <div className="m-16 p-16">
       <Button className="m-16">Next link button</Button>
       <Text className="mt-10">Test</Text>
     </div>
