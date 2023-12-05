5b0a05a57e114a6c95c7aaf2260a8336356de3b6
urquico <kurtjacob.urquico@gmail.com>
2023-12-05 20:58:01 +0800

    [Feature][Kurt] - Fix loader and root route

--- src/app/me/loading.tsx
+++ src/app/me/loading.tsx
@@ -1,10 +1,16 @@
+import { MantineProvider, Text } from '@mantine/core';
 import { IconLoader2 } from '@tabler/icons-react';
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <main className='w-full h-screen flex justify-center items-center'>
-      <IconLoader2 color='gray' className='w-10 h-10 animate-spin' />
+    <main className='w-full h-screen flex flex-col place-content-center items-center animate-pulse'>
+      <IconLoader2 color='gray' className='w-10 h-10 animate-spin mb-4' />
+      <MantineProvider>
+        <Text className='text-white text-base font-bold animate-bounce'>
+          Loading
+        </Text>
+      </MantineProvider>
     </main>
   );
 }
