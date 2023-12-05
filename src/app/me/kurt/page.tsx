2eecbbafde9899b5c128a4586e49180e29f42492
urquico <44643247+urquico@users.noreply.github.com>
2023-12-06 01:36:34 +0800

    [Feature][Kurt] - Button fix and Mobile Banner adjustments (#9)
    * [Feature][Kurt] - Changed button layout for socials
    
    * [Feature][Kurt] - Minor changes on the banner height
    
    * [Refactor][Kurt] - Created banner.tsx for banner component

--- src/app/me/kurt/page.tsx
+++ src/app/me/kurt/page.tsx
@@ -1,45 +1,19 @@
 import AvatarProfile from '@/components/avatar-profile';
+import Banner from '@/components/banner';
 import ButtonLinks from '@/components/button-links';
-import { BackgroundImage, Overlay, Text } from '@mantine/core';
 import React from 'react';
 
 function Home() {
   return (
     <main className='flex flex-col place-items-center'>
-      <BackgroundImage
-        className='h-[70vh] max-md:h-[50vh] w-screen rounded-[3rem] bg-top  max-md:bg-[center_bottom_9rem] blur-3xl bg-fixed flex justify-end'
-        src='/kid-a.png'
-      >
-        <Overlay
-          gradient='linear-gradient(145deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.95) 100%)'
-          opacity={0.85}
-          className='h-[70vh] max-md:h-[50vh] w-screen rounded-[3rem] blur-3xl bg-fixed z-30'
-        />
-        <Text
-          component='a'
-          href='https://www.reddit.com/r/radiohead/comments/14cnhhz/animated_kid_a_wallpaper_in_4k_zoomed_out_with_ai/'
-          target='_blank'
-          fz='sm'
-          className='place-self-end mr-10 mb-4 z-40 text-neutral-400 hover:underline hover:text-neutral-200 max-md:hidden'
-        >
-          Kid A Album by Radiohead expanded by AI
-        </Text>
-      </BackgroundImage>
+      {/* Banner */}
+      <Banner />
 
       {/* Avatar */}
       <AvatarProfile />
 
       {/* Socials */}
       <ButtonLinks />
-
-      {/* Stack */}
-      {/* <div className='w-[40vw] grid grid-cols-4 gap-4 mx-20 my-10 justify-items-center'>
-        {stackData.map((stack) => {
-          return (
-            <Stack src={stack.src} label={stack.label} key={stack.label} />
-          );
-        })}
-      </div> */}
     </main>
   );
 }
