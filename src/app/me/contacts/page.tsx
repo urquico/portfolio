0b25b50a325880b35db7dfcedcd6ed72f7b72f09
urquico <kurtjacob.urquico@gmail.com>
2023-12-06 02:46:11 +0800

    [Feature][Kurt] - Added maintenance mode

--- src/app/me/contacts/page.tsx
+++ src/app/me/contacts/page.tsx
@@ -1,7 +1,18 @@
+import Maintenance from '@/components/maintenance';
+import { contactMaintenance } from '@/lib/constants/maintenance-message';
 import React from 'react';
 
 function page() {
-  return <div>page</div>;
+  return (
+    <main>
+      <Maintenance
+        emoji={contactMaintenance.emoji}
+        title={contactMaintenance.title}
+        subtitle={contactMaintenance.subtitle}
+        description={contactMaintenance.description}
+      />
+    </main>
+  );
 }
 
 export default page;
