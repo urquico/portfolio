a365734e456ba4a1fe0c33f982264b28f7120bd4
urquico <44643247+urquico@users.noreply.github.com>
2023-12-05 20:59:52 +0800

    [Feature][Kurt] - Added Loader (#5)
    * [Fix][Kurt] - Fix hydration abug
    
    * [Feature][Kurt] - Fix loader and root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import { Text } from '@mantine/core';
-import React from 'react';
+import { redirect } from 'next/navigation';
 
 function Home() {
-  return (
-    <div className='w-screen h-screen flex justify-center place-items-center'>
-      <Text component='a' href='/me/kurt' c='white' fz='xl'>
-        Continue
-      </Text>
-    </div>
-  );
+  return redirect('/me/kurt');
 }
 
 export default Home;
