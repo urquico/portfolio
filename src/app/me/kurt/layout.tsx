e192ced1ecf697dae5cf40eb632c4920005f1b90
urquico <44643247+urquico@users.noreply.github.com>
2023-12-05 21:04:46 +0800

    [Build] - Added Loader (#6)
    * [Feature][Kurt] - Added mantine 7
    
    * [Bug][Kurt] - Bug on tw and mantine
    
    * [Feature][Kurt] - Added next turbo pack
    
    * [Feature][Kurt] - Added prettier and eslints
    
    * [Fix][Kurt] - Fixed prettier bug
    
    * [Misc][Kurt] - Removed all files
    
    * [Misc][Kurt] - Installed NextJS
    
    * [Fix][Kurt] - Fixed tw bug with mantine
    
    * [Feature][Kurt] - Added Sweep AI
    
    * [Feature][Kurt] - Added Home Page (#3)
    
    * [Setup] - Installed Mantine, NextJS, Tailwind (#2)
    
    * [Feature][Kurt] - Added mantine 7
    
    * [Bug][Kurt] - Bug on tw and mantine
    
    * [Feature][Kurt] - Added next turbo pack
    
    * [Feature][Kurt] - Added prettier and eslints
    
    * [Fix][Kurt] - Fixed prettier bug
    
    * [Misc][Kurt] - Removed all files
    
    * [Misc][Kurt] - Installed NextJS
    
    * [Fix][Kurt] - Fixed tw bug with mantine
    
    * [Feature][Kurt] - Added Sweep AI
    
    * [Feature][Kurt] - Added Basic Homepage layout
    
    * [Feature][Kurt] - Added basic navigation and stack component
    
    * [Feature][Kurt] - Added label kid a and basic nav
    
    * [Feature][Kurt] - Added Navigation layout
    
    * [Feature][Kurt] - Added right icon
    
    * [Feature][Kurt] - Added responsive layout for avatar and banner
    
    * [Feature][Kurt] - Added responsive layout for socials
    
    * [Feature][Kurt] - Added Typing Animation on Tooltip
    
    * [Feature][Kurt] - Added responsive layout for navigation
    
    * [Fix][Kurt] - Fix vercel build
    
    * [Feature][Kurt] - Added Loader (#5)
    
    * [Fix][Kurt] - Fix hydration abug
    
    * [Feature][Kurt] - Fix loader and root route

--- src/app/me/kurt/layout.tsx
+++ src/app/me/kurt/layout.tsx
@@ -2,19 +2,14 @@ import type { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: 'Urquico',
+  title: 'Kurt | Dev',
   description: 'Portfolio',
 };
 
-export default function RootLayout({
+export default function KurtLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang='en'>
-      <head></head>
-      <body>{children}</body>
-    </html>
-  );
+  return <main>{children}</main>;
 }
