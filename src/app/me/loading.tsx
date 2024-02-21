3c6ef2450322d62126095f78c36703793fbb2940
urquico <kurtjacob.urquico@gmail.com>
2024-02-22 02:15:30 +0800

    [Feature][Kurt] - Changed loader to fidget spinner

--- src/app/me/loading.tsx
+++ src/app/me/loading.tsx
@@ -1,11 +1,11 @@
 import { MantineProvider, Text } from '@mantine/core';
-import { IconLoader2 } from '@tabler/icons-react';
+import { IconFidgetSpinner } from '@tabler/icons-react';
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <main className='w-full h-screen flex flex-col place-content-center items-center animate-pulse'>
-      <IconLoader2 color='gray' className='w-10 h-10 animate-spin mb-4' />
+      <IconFidgetSpinner color='gray' className='w-10 h-10 animate-spin mb-4' />
       <MantineProvider>
         <Text className='text-white text-base font-bold animate-bounce'>
           Loading
