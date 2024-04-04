963d28c14e5954d18f080ea5ccf5bddaf3dd1740
urquico <kurtjacob.urquico@gmail.com>
2024-04-04 19:52:56 +0800

    [Feature][Kurt] - Bind tabs to URL

--- src/app/me/about/page.tsx
+++ src/app/me/about/page.tsx
@@ -1,48 +1,7 @@
-import TabsDefault from '@/components/tabs-default';
-
-import AboutSection from './about-section';
-import ExperienceTimeline from './experience-timeline';
-import NameDisplay from './name-display';
-import SkillFocus from './skill-focus';
+import { redirect } from 'next/navigation';
 
 function page() {
-  const tabList = [
-    {
-      value: 'general',
-      label: 'General Info',
-      icon: '👨‍💻',
-      content: (
-        <>
-          {/* About Me */}
-          <AboutSection />
-
-          {/* Skills & Focus */}
-          <SkillFocus />
-        </>
-      ),
-    },
-    {
-      value: 'experience',
-      label: 'Experience',
-      icon: '🏢',
-      content: (
-        <>
-          <ExperienceTimeline />
-        </>
-      ),
-    },
-  ];
-
-  return (
-    <main className='flex flex-col items-center'>
-      {/* Name */}
-      <NameDisplay />
-
-      <section className='flex justify-center w-full'>
-        <TabsDefault tabList={tabList} className='mt-8' />
-      </section>
-    </main>
-  );
+  return redirect('/me/about/general-info');
 }
 
 export default page;
---
+++ src/app/me/about/work-experience/page.tsx
@@ -0,0 +1,13 @@
+import ExperienceTimeline from '../experience-timeline';
+import Testimonials from '../testimonials';
+
+function page() {
+  return (
+    <>
+      <ExperienceTimeline />
+      <Testimonials />
+    </>
+  );
+}
+
+export default page;
