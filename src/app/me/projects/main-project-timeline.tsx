90d6ecda05331cce5d47e98d56916ca22e7e8016
urquico <44643247+urquico@users.noreply.github.com>
2024-04-21 03:15:55 +0800

    [Fix][Kurt] - Deployment Error

--- src/app/me/projects/main-project-timeline.tsx
+++ src/app/me/projects/main-project-timeline.tsx
@@ -1,5 +1,5 @@
 import CustomTimeline from '@/components/custom-timeline';
-import { ProjectData } from '@/lib/constants/project-data';
+import { ProjectData } from '@/lib/types';
 
 function MainProjectTimeline({ projectData }: { projectData: ProjectData[] }) {
   return <CustomTimeline projectData={projectData} />;
